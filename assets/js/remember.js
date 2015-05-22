2973294ead345fa8589571de9c930ae09a2ca06d
alpinus <dongcai.zhao@outlook.com>
2015-05-22 08:55:20 +0800

    add some new images
    learn funcion added

--- assets/js/remember.js
+++ assets/js/remember.js
@@ -1,6 +1,18 @@
+var total_id=10;
+var current_id=1;
 function init(){
 	//alert("init");
+	show_hiragana();
 }
 function next(){
-	alert("next");
+	if(current_id+1<total_id){
+		current_id++;
+	}else{
+		current_id=1;	
+	}
+	show_hiragana();
+}
+function show_hiragana() {
+	var img= $(".hiragana-img");
+	img.attr("src","assets/image/hiragana/"+current_id+".png");
 }
